b33bcd565a68003e7df54ccaeab89eb6744fa8f9
Tim Smith <tsmith@chef.io>
2017-05-10 21:13:19 -0700

    Resolve cookstyle warnings and use converge_by / inline resources
    This gets this cookbook closer to working on Chef 13:
    
    use_inline_resources should be used everywhere and it's on by default in chef 13
    Turn on why-run mode which is also on by default in chef 13
    Stop using     new_resource.updated_by_last_action(true) which throws a Foodcritic warning and has been replaced by converge_by. Converge_by handles all the notifications and also logging / reporting to Automate
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- beadm/providers/default.rb
+++ beadm/providers/default.rb
@@ -16,6 +16,12 @@
 
 include Chef::Mixin::ShellOut
 
+use_inline_resources
+
+def whyrun_supported?
+  true
+end
+
 def load_current_resource
   @beadm = Chef::Resource::Beadm.new(new_resource.name)
   @beadm.name(new_resource.name)
@@ -27,139 +33,124 @@ def load_current_resource
   @beadm.current_props(current_props?)
 end
 
-
-
 action :create do
   unless created?
-    Chef::Log.info("Creating beadm #{@beadm.name}")
-    beadm_options = ""
-    if (@beadm.options.empty?) 
-    	Chef::Log.info("beadm options not passed")
-    else
-    	Chef::Log.info("beadm options passed")
-      @beadm.options.each do |key,val|
-        tmp = "-o #{key}=#{val} "
-        beadm_options = beadm_options + tmp
+    converge_by "create beadm #{@beadm.name}" do
+      beadm_options = ''
+      if @beadm.options.empty?
+        Chef::Log.info('beadm options not passed')
+      else
+        Chef::Log.info('beadm options passed')
+        @beadm.options.each do |key, val|
+          tmp = "-o #{key}=#{val} "
+          beadm_options += tmp
+        end
       end
-    end
 
-    shell_out!("beadm create #{@beadm.name}")
-    
-    if @beadm.activate == true
-      shell_out!("beadm activate #{@beadm.name}")
-    end
-    # update properties for new zfs
-    @beadm.info(info?)
-    @beadm.current_props(current_props?)
+      shell_out!("beadm create #{@beadm.name}")
 
-    new_resource.updated_by_last_action(true)
+      shell_out!("beadm activate #{@beadm.name}") if @beadm.activate == true
+      # update properties for new zfs
+      @beadm.info(info?)
+      @beadm.current_props(current_props?)
+    end
   end
-
 end
 
 action :destroy do
   if created?
-    Chef::Log.info("Destroying beadm with beadm destroy -F #{@beadm.name}")
-    shell_out!("beadm destroy -F #{@beadm.name}")
-    new_resource.updated_by_last_action(true)
+    converge_by "destroy beadm #{@beadm.name}" do
+      shell_out!("beadm destroy -F #{@beadm.name}")
+    end
   end
 end
 
 action :destroy_pattern do
-    Chef::Log.info("Destroying beadm with #{@beadm.name}")
   prop_hash = {}
   @beadm.list.stdout.split("\n").each do |line|
-    l = line.split(';');
+    l = line.split(';')
     prop_hash[l[0]] = line
   end
-  prop_hash.each do |key, array|
-      bename = key
-      if ( bename =~ /#{@beadm.name}/ )
-        shell_out!("beadm destroy -F #{key}")
-        new_resource.updated_by_last_action(true)
-      end
+  prop_hash.each do |key, _array|
+    bename = key
+    next unless bename =~ /#{@beadm.name}/
+    converge_by "destroy_pattern beadm #{@beadm.name}" do
+      shell_out!("beadm destroy -F #{key}")
+    end
   end
 end
 
-
 action :activate do
   if created?
-    Chef::Log.info("Activate beadm with beadm activate #{@beadm.name}")
-    shell_out!("beadm activate #{@beadm.name}")
-    new_resource.updated_by_last_action(true)
+    converge_by "active beadm with beadm activate #{@beadm.name}" do
+      shell_out!("beadm activate #{@beadm.name}")
+    end
   end
 end
 
 action :beadm_publisher_set do
   if created?
-    Chef::Log.info("Mount beadm with beadm mount #{@beadm.name}")
-    shell_out!("beadm activate #{@beadm.name}")
-    new_resource.updated_by_last_action(true)
+    converge_by "mount beadm with beadm mount #{@beadm.name}" do
+      shell_out!("beadm activate #{@beadm.name}")
+    end
   end
 end
 
 action :mount do
-flag = -1 
+  flag = -1
   if created?
-	current_be_props = @beadm.current_props[@beadm.name]
-    tmp = current_be_props.split(';');
-    if tmp[3] == @beadm.mountpoint 
-    	Chef::Log.info("BE already mounted in #{tmp[3]}")
-		flag = 0
-	else 
-    	if tmp[3] != @beadm.mountpoint && tmp[3] != ''
-    		Chef::Log.info("BE already mounted. Unmount #{tmp[3]}")
-			flag = 1
-			raise "BE #{@beadm.name} is already mounted in #{tmp[3]}. Please unmount before proceeding."
-   		else	 
-    		Chef::Log.info("Mount #{@beadm.name} with mountpoint #{@beadm.mountpoint}")
-    		status = shell_out!("beadm mount #{@beadm.name} #{@beadm.mountpoint}")
-			flag = status.exitstatus
-    		new_resource.updated_by_last_action(true)
-		end
-  	end
+    current_be_props = @beadm.current_props[@beadm.name]
+    tmp = current_be_props.split(';')
+    if tmp[3] == @beadm.mountpoint
+      Chef::Log.info("BE already mounted in #{tmp[3]}")
+      flag = 0
+    elsif tmp[3] != @beadm.mountpoint && tmp[3] != ''
+      Chef::Log.info("BE already mounted. Unmount #{tmp[3]}")
+      flag = 1
+      raise "BE #{@beadm.name} is already mounted in #{tmp[3]}. Please unmount before proceeding."
+    else
+      converge_by "mount #{@beadm.name} with mountpoint #{@beadm.mountpoint}" do
+        status = shell_out!("beadm mount #{@beadm.name} #{@beadm.mountpoint}")
+        flag = status.exitstatus
+      end
+    end
   end
-	
-	Chef::Log.info("Mount return value #{flag}")
-	flag 
+
+  Chef::Log.info("Mount return value #{flag}")
+  flag
 end
 
 action :umount do
   if created?
     unless already_mounted?
-    Chef::Log.info("Umount beadm with beadm umount #{@beadm.name}")
-    shell_out!("beadm umount #{@beadm.name}")
-    new_resource.updated_by_last_action(true)
+      converge_by "unmount beadm with beadm umount #{@beadm.name}" do
+        shell_out!("beadm umount #{@beadm.name}")
+      end
     end
   end
 end
 
 action :rename do
   if created?
-    Chef::Log.info("Renaming beadm #{@beadm.name} to #{@beadm.new_be}")
-    shell_out!("beadm rename #{@beadm.name} #{@beadm.new_be}")
-    new_resource.updated_by_last_action(true)
+    converge_by "rename #{@beadm.name} to #{@beadm.new_be}" do
+      shell_out!("beadm rename #{@beadm.name} #{@beadm.new_be}")
+    end
   end
 end
-
 
 private
 
 def created?
-  Chef::Log.info("Checking beadm already created")
-  flag = @beadm.info.exitstatus.zero?
-  if node['platform_version'] == '5.11'
-    if @beadm.info.stdout.lines.count == 0
-      flag = false
-    end
-  end
+  Chef::Log.info('Checking beadm already created')
+  flag = @beadm.info.exitstatus == 0
+  flag = false if node['platform_version'] == '5.11' && @beadm.info.stdout.lines.count == 0
   flag
 end
 
 def current_props?
   prop_hash = {}
   @beadm.info.stdout.split("\n").each do |line|
-    l = line.split(';');
+    l = line.split(';')
     prop_hash[l[0]] = line
   end
   prop_hash
@@ -171,6 +162,6 @@ def info?
 end
 
 def list?
-  Chef::Log.info("Checking beadm list -H")
-  shell_out("beadm list -H")
+  Chef::Log.info('Checking beadm list -H')
+  shell_out('beadm list -H')
 end
